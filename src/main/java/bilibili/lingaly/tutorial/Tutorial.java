06edd494843ca995b43f1c7c02485a68bd9874e6
Ling-ALY <yxp20050828@163.com>
2022-05-09 17:23:32 +0800

    去除了对easyfabricmodregister的依赖

--- src/main/java/bilibili/lingaly/tutorial/Tutorial.java
+++ src/main/java/bilibili/lingaly/tutorial/Tutorial.java
@@ -1,6 +1,5 @@
 package bilibili.lingaly.tutorial;
 
-import bilibili.lingaly.fabricmodregister.JsonCreater;
 import bilibili.lingaly.tutorial.block.ModBlocks;
 import bilibili.lingaly.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
@@ -8,11 +7,6 @@ import net.fabricmc.api.ModInitializer;
 public class Tutorial implements ModInitializer {
 
     public static String MOD_ID = "tutorial";
-
-    public static void main(String[] args) {
-        JsonCreater.createItemJson(MOD_ID, "fooda", "食物a");
-        JsonCreater.createBlockJson(MOD_ID, "tutorial_block", "教程块");
-    }
 
     @Override
     public void onInitialize() {
