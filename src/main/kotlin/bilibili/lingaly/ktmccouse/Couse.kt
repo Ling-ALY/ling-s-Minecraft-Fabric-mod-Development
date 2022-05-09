d0ef501d434bb8b22e44c321bee0ba05de4bebc8
Ling-ALY <yxp20050828@163.com>
2022-05-09 20:56:38 +0800

    首次提交(第零课进度)

---
+++ src/main/kotlin/bilibili/lingaly/ktmccouse/Couse.kt
@@ -0,0 +1,10 @@
+package bilibili.lingaly.ktmccouse
+
+import net.fabricmc.api.ModInitializer
+
+object Couse: ModInitializer {
+    public const val MOD_ID = "ktmccouse"
+
+    override fun onInitialize() {
+    }
+}
